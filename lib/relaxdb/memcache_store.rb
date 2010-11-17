4ec8560d895b53e2c0e05a069bccec04caad0f6e
Frederick Cheung <frederick.cheung@gmail.com>
2010-11-17 19:52:30 +0000

    autfix the keys ourself - dalli doesn't

--- lib/relaxdb/memcache_store.rb
+++ lib/relaxdb/memcache_store.rb
@@ -1,5 +1,6 @@
 gem 'dalli'
 require 'zlib'
+require 'digest/sha1'
 module RelaxDB
   class MemcacheStore
     attr_reader :cache
@@ -13,10 +14,12 @@ module RelaxDB
     end
     
     def get_etag(cache_key)
+      key = check_key(cache_key)
       @cache.get "etag:#{cache_key}"
     end
     
     def get_data(cache_key)
+      key = check_key(cache_key)
       entry = @cache.get "data:#{cache_key}"
       return nil if !entry
       if entry[:deflated]
@@ -27,6 +30,7 @@ module RelaxDB
     end
 
     def store(cache_key, data, etag)
+      key = check_key(cache_key)
       deflated = false
       if data.length >= 1024 * 1024
         data = Zlib::Deflate.deflate(data, 1)
@@ -36,6 +40,14 @@ module RelaxDB
       if data.size < 1024*1024 #memcache cannot store values over 1mb
         @cache.set( "data:#{cache_key}", {:data => data, :deflated => deflated})
         @cache.set( "etag:#{cache_key}", etag)
+      end
+    end
+    
+    def check_key(key)
+      if key =~ /\s/ || key_length(key) > 250
+        key = "#{Digest::SHA1.hexdigest(key)}-autofixed"
+      else
+        key
       end
     end
   end
