5b4cff0c980cd84feefa71c65098cdc426f781d3
Frederick Cheung <frederick.cheung@gmail.com>
2010-11-17 20:01:54 +0000

    fix autofix keys

--- lib/relaxdb/memcache_store.rb
+++ lib/relaxdb/memcache_store.rb
@@ -14,12 +14,12 @@ module RelaxDB
     end
     
     def get_etag(cache_key)
-      key = check_key(cache_key)
+      cache_key = check_key(cache_key)
       @cache.get "etag:#{cache_key}"
     end
     
     def get_data(cache_key)
-      key = check_key(cache_key)
+      cache_key = check_key(cache_key)
       entry = @cache.get "data:#{cache_key}"
       return nil if !entry
       if entry[:deflated]
@@ -30,7 +30,7 @@ module RelaxDB
     end
 
     def store(cache_key, data, etag)
-      key = check_key(cache_key)
+      cache_key = check_key(cache_key)
       deflated = false
       if data.length >= 1024 * 1024
         data = Zlib::Deflate.deflate(data, 1)
@@ -44,7 +44,7 @@ module RelaxDB
     end
     
     def check_key(key)
-      if key =~ /\s/ || key_length(key) > 250
+      if key =~ /\s/ || key.length > 250
         key = "#{Digest::SHA1.hexdigest(key)}-autofixed"
       else
         key
